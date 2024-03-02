fb98a890af08e4418e63d33b877a36b6fb541643
rohith887 <rohithreddy1325@gmail.com>
2024-03-02 18:56:41 +0530

    init

---
+++ j1.java
@@ -0,0 +1,39 @@
+import java.util.*;
+import java.util.Scanner;
+public class j1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int SIZE = 3; // Define the size of arrays
+        int[] a = new int[SIZE];
+        int[] b = new int[SIZE];
+
+        // Input elements for array a
+        for (int i = 0; i < SIZE; i++) {
+            a[i] = sc.nextInt();
+        }
+
+        // Input elements for array b
+        for (int i = 0; i < SIZE; i++) {
+            b[i] = sc.nextInt();
+        }
+
+        int ascore = 0, bscore = 0;
+
+        // Comparing elements of arrays and updating scores
+        for (int i = 0; i < SIZE; i++)
+        {
+            if (a[i] > b[i]) 
+            {
+                ascore++;
+            } 
+            else if (a[i] < b[i]) 
+            {
+                bscore++;
+            }
+        }
+
+        // Printing scores
+        System.out.println(ascore + " " + bscore);
+    }
+}
+
